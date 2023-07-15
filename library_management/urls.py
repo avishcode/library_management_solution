965124107aa29755c2fdf75774ddf1805aa6481f
vishavant <officialvishavant@gmail.com>
2023-07-15 14:09:24 +0530

    homepage added

--- library_management/urls.py
+++ library_management/urls.py
@@ -21,5 +21,6 @@ from library_management import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('website.urls', namespace='website')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
                                                                                          document_root=settings.STATIC_ROOT)
